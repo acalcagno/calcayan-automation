544a6f1d5ff13d8f1d21a214831826781404f568
acalcagno <calqui@gmail.com>
2018-03-10 03:22:23 -0300

    metodos get y post

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json'}));
 
-app.post('/', function (req, res) {
+app.get('/', function (req, res) {
     res.send("post");
 });
 
