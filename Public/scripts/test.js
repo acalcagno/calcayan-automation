9bfee8d239aa0313d91bdd2f3e275021f1cd65cb
acalcagno <calqui@gmail.com>
2018-04-15 22:47:47 -0300

    se envia ajax call al presionar un boton del panel de control

--- Public/scripts/test.js
+++ Public/scripts/test.js
@@ -1,4 +1,19 @@
-$('.acciona_backend').on('click', function() {
-    //postItem();
-    alert("test");
+$('.acciona_backend').on('click', function(e) {
+    e.preventDefault();
+    console.log('select_link clicked');
+    var btn = e.target.attributes;
+
+
+    var data = {dispositivo: btn.dispositivo.value, accion:btn.accion.value };
+
+    $.ajax({
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+        url: 'http://localhost:5000/control',
+        success: function(data) {
+            console.log('success');
+            console.log(JSON.stringify(data));
+        }
+    });
 });
