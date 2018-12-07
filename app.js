a4f7764d98bee03db7cbe58fd83ca2a85ca8bd78
acalcagno <calqui@gmail.com>
2018-12-07 12:49:04 -0300

    log apertura ev

--- app.js
+++ app.js
@@ -52,7 +52,6 @@ app.post('/mediciones', function(req, res) {
 update_dispositivos = function(acciones, mediciones, dispositivos) {
     var a = acciones
 
-
     //seteo las nuevas acciones tomadas
     _.each(acciones, a =>  {
         var disp = _.find(dispositivos, d => d.dispositivo == a.dispositivo)
@@ -76,9 +75,6 @@ update_dispositivos = function(acciones, mediciones, dispositivos) {
             }
         }
     })
-
-
-
 }
 
 error_mediciones = function() {
@@ -93,13 +89,13 @@ calcular_respuesta_para_el_procesador = function(mediciones, next, err_callback)
             console.log(err)
             err_callback()
         } else {
-            var acciones = get_acciones(dispositivos)
+            var acciones = get_acciones(dispositivos, mediciones)
             next(acciones, mediciones, dispositivos)
         }
     })
 }
 
-agregar_accion_en_la_base = function(acciones, dispositivos, nombre_dispositivo) {
+agregar_accion_guardada_en_la_base = function(acciones, dispositivos, nombre_dispositivo) {
     acciones.push({"dispositivo": nombre_dispositivo, "accion": _.find(dispositivos, d => d.dispositivo == nombre_dispositivo).accion});
 }
 
@@ -107,20 +103,20 @@ agregar_accion_nula = function(acciones, nombre_dispositivo) {
     acciones.push({"dispositivo": nombre_dispositivo, "accion": 0} );
 }
 
-get_acciones = function(dispositivos) {
+get_acciones = function(dispositivos, mediciones) {
     var acciones = []
 
-    agregar_accion_en_la_base(acciones, dispositivos, "electrovalvula_frio_fermentador_1")
+    configurar_ev(acciones, dispositivos, mediciones, "electrovalvula_frio_fermentador_1")
     agregar_accion_nula(acciones, "electrovalvula_calor_fermentador_1")
 
-    agregar_accion_en_la_base(acciones, dispositivos, "electrovalvula_frio_fermentador_2")
+    agregar_accion_guardada_en_la_base(acciones, dispositivos, "electrovalvula_frio_fermentador_2")
     agregar_accion_nula(acciones, "electrovalvula_calor_fermentador_2")
 
-    agregar_accion_en_la_base(acciones, dispositivos, "electrovalvula_frio_fermentador_3")
+    agregar_accion_guardada_en_la_base(acciones, dispositivos, "electrovalvula_frio_fermentador_3")
     agregar_accion_nula(acciones, "electrovalvula_calor_fermentador_3")
 
-    agregar_accion_en_la_base(acciones, dispositivos, "chiller")
-    agregar_accion_en_la_base(acciones, dispositivos, "bomba_chiller")
+    agregar_accion_guardada_en_la_base(acciones, dispositivos, "chiller")
+    agregar_accion_guardada_en_la_base(acciones, dispositivos, "bomba_chiller")
 
     agregar_accion_nula(acciones, "calentador")
     agregar_accion_nula(acciones, "bomba_calentador")
@@ -128,6 +124,46 @@ get_acciones = function(dispositivos) {
     return acciones
 }
 
+configurar_ev = function (acciones, dispositivos, mediciones, nombre_ev) {
+    var ev = _.find(dispositivos, d => d.dispositivo == nombre_ev)
+    if (!ev) {
+        console.log('no se encontro el dispositivo ' + nombre_ev + ' en la base de datos')
+    } else {
+        if (ev.control == "manual") {
+            agregar_accion_guardada_en_la_base(acciones, dispositivos, nombre_ev)
+        } else {
+            var fermentador = fermentador_de(nombre_ev, dispositivos)
+            if (!fermentador) {
+                console.log('no se encontro el fermentador correspondiente a la ev ' + nombre_ev + ' en la db')
+            } else {
+                var medicion_fermentador = _.find(mediciones, m => m.sensor == fermentador.dispositivo)
+                if (!medicion_fermentador) {
+                    console.log('no se encontro la medicion para el fermentador ' + fermentador.dispositivo)
+                } else {
+                    if(medicion_fermentador.temperatura > fermentador.temp_ideal + fermentador.tolerancia) {
+                        var medicion_chiller = _.find(mediciones, mc => mc.sensor == "chiller")
+                        if(!medicion_chiller) {
+                            console.log('no se encontro la medicion del chiller')
+                        } else {
+                            if(medicion_chiller.temperatura >= medicion_fermentador.temperatura ) {
+                                console.log('el fermentador ' + fermentador.dispositivo + ' necesita frio pero el chiller está mas caliente')
+                            } else {
+                                console.log('deberia abrirse ev')
+                            }
+                        }
+                    }
+                    if(medicion_fermentador.temperatura < fermentador.temp_ideal - fermentador.tolerancia) {
+                        console.log('deberia cerrarse ev')
+                    }
+                }
+            }
+        }
+    }
+}
+
+fermentador_de = function(nombre_ev, dispositivos) {
+    return _.find(dispositivos, d => d.dispositivo == "fermentador" + nombre_ev.substring(nombre_ev.length-1,nombre_ev.length))
+}
 
 function log(req, res, next) {
     console.log(req.body)
